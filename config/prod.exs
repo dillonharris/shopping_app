e59f7af2b10b8e91c13cd26b88e21a9ee92e7a2a
Dillon <harrisdillon0@gmail.com>
2019-04-15 12:49:50 +0200

    update prod config

--- config/prod.exs
+++ config/prod.exs
@@ -23,7 +23,7 @@ config :shopping_app, ShoppingAppWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :shopping_app, ElixirTestApp.Repo,
+config :shopping_app, ShoppingApp.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
